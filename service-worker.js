227ce8f6e46c00a7928dc3efcbe8f50dd7f406fd
adamonine-stack <adamo.nine@gmail.com>
2026-06-30 02:50:13 +0900

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,3 +0,0 @@
-const CACHE='r32-v3-complete';
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','index.html','css/styles.css','js/app.js','assets/r32-wallpaper.png','icons/icon-192.png'])))});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
